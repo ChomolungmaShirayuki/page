1ce9e00c104fc00d0155798656e2e8048e6af1a9
ChomolungmaShirayuki <remiria.karisuma@live.jp>
2022-07-27 16:29:42 +0900

    up

--- js/index.js
+++ js/index.js
@@ -3,15 +3,21 @@ console.log("test");
 
 
 let once01 = false;
-	window.addEventListener('scroll', function () {
-		
-		// ターゲットの画面トップからの距離
-		taeget_position01 = document.querySelector('.box').getBoundingClientRect().top;
-		
-		// 画面トップからの距離から画面の高さより小さければ実行する
-			if (taeget_position01 <= window.innerHeight && once01 !== true) {
-				once01 = true;
-                //cssでアニメーションを追加
-				document.querySelector(".box").style.animation = "SlideIn 2s";
-			}
+window.addEventListener('scroll', function () {
+
+	// ターゲットの画面トップからの距離
+	taeget_position01 = document.querySelector('.box').getBoundingClientRect().top;
+
+	// 画面トップからの距離から画面の高さより小さければ実行する
+	if (taeget_position01 <= window.innerHeight && once01 !== true) {
+		once01 = true;
+		//cssでアニメーションを追加
+		document.querySelector(".box").style.animation = "SlideIn 2s";
+	}
+});
+
+$(function () {
+	$(".test").on("inview", function () {
+		$(this).addClass("is-inview");
 	});
+});
